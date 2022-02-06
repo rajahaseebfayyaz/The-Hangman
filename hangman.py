9a5aa2941d19a0cb9c9431bf135a23b252a2a093
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2022-02-06 01:24:58 +0000

    created guidsnce and navigatioon for user

--- hangman.py
+++ hangman.py
@@ -5,5 +5,13 @@ def get_word():
   word = random.choice(word_list)
   return word.upper()
 
-  def play (word):
+  def play(word):
       word_completion = "_" * len(word)
+      guessed = False
+      guessed_letters = []
+      guessed_words = []
+      tries = 6
+      print("Let's play Hangman!")
+      print(display_hangman(tries))
+      print(word_completion)
+      print("/n") 
