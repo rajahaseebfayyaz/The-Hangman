45d16d0be2e7d3710f27b8b4794475c6cdd6bb1c
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2022-02-08 20:03:00 +0000

    created hangman visualisation and entered more variables

--- hangman.py
+++ hangman.py
@@ -28,14 +28,177 @@ def get_word():
                           print("Good job,", guess, "is i the word!")
                           guessed_letters.append(guess)
                           word_as_list = list(word_completion)
+                          indices = [i for i, letter in enumerate(word) if letter == guess]
+                          for index in indices:
+                              word_as_list[index] = guess
+                              word_completion = "".join(word_as_list)
+                              if "_" not in word_completion:
+                                  guessed = true
 
                       elif len(guess) == len(word) and guess.isalpha():
+                          if guess in guessed_words:
+                              print("You already guessed the word", guess)
+                              elif guess!= word:
+                                  print(guess, "is not the word.")
+                                  tries -= 1
+                                  guessed_words.append (guess)
+                                  else:
+                                      guessed = True
+                                      word_completion = word
+
+
 
                   else:
                       print("Not a valid guess.")
                       print(display_hangman(tries))
                       print(word_completion)
                       print("/n")
+
+                      if guessed:
+                          print("Congrats, you guessed the word! You win!")
+                          else:
+                              print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+
+
+                              def display_hangman(tries):
+
+                                  stages = [ """
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  | | 
+|  | | 
+|
+--------
+""")
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  | | 
+|  | 
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  | 
+|  | 
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|  |
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|   | 
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|   | 
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-\ 
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+| /-+-
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+|  -+-
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|   0
+|
+|
+|
+|
+|
+|
+--------
+""",
+"""
+-----
+|   |
+|
+|
+|
+|
+|
+|
+|
+--------
+""",
+]
+return stages[tries]
+
+
+def main()
 
                       
  
