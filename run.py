2989b1e4c3199e73a835754931cdab8207a25013
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2022-04-08 11:46:11 +0000

    code erros fixed and commenting done

--- run.py
+++ run.py
@@ -1,89 +1,140 @@
-def main():
-    print("WELCOME TO HANGMAN")
-    word = get_word()
-    play(word)
+import random
+from words import words
+from colorama import Fore, Style
 
-if __name__ == "__main__":
-    main()
+# The idea of this game and bit of code has been taken from youtube tutorials and from emmalawlor hangman game  # noqa
 
-    import random
-from words import word_list, stages
 
-# The idea and the bit of code for this game was taken from emmalawlor also from youtube tutorials # noqa
 
 class Hangman:
     """
-    This proejct is an example of the original hangman game
-    all the features required for gameplay are included
+    Creates an instance of the hangman game
+    Initializes all attributes required for gameplay
     Displays the hangman to the user
     Runs the game by asking the user for input
     Evaluates user input
     Determines result & checks if user wants to play again
     """
+    def __init__(self):
+        self.word = random.choice(words)
+        self.stage = 0
+        self.guessed_letters = []
+        self.guessed_words = []
+        # Solution for displaying the hidden word taken from this tutorial https://www.youtube.com/c/KiteHQ  # noqa
+        # With underscores replaced by dashes
+        self.progress = '-' * len(self.word)
+        self.games_played = 0
+        self.games_won = 0
 
-def get_word():
-    print("finding the word... ")
-    word = random.choice(word_list)
-    return word.upper()
+    def display_hangman(self):
+        """
+        Displays the hangman image relevant to the amount of lives remaining
+        Dsiplays the random word with letters represented by dashes
+        """
+        print(Fore.CYAN + HANGMAN_PICS[self.stage])
+        print('\n')
+        print(self.progress + Style.RESET_ALL)
+        print('\n')
 
-
-def play(word):
-    """This function initializes the word completion"""
-    print("YOU ARE IN PLAY")
-    word_completion = "_" * len(word)
-    guessed = False
-    guessed_letters = []
-    guessed_words = []
-    tries = 6
-    print("Let's play Hangman!")
-    print(display_hangman(tries))
-    print(word_completion)
-    print("/n")
-    while not guessed and tries > 0:
-        guess = input("Please guess a letter or word: ").upper()
-        if len(guess) == 1 and guess.isalpha():
-            if guess in guessed_letters:
-                print("You already guessed the letter", guess)
-            elif guess not in word:
-                print(guess, "is not in the word.")
-                tries -= 1
-                guessed_letters.append(guess)
-            else:
-                print("Good job,", guess, "is i the word!")
-                guessed_letters.append(guess)
-                word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(
-                    word) if letter == guess]
-                for index in indices:
-                    word_as_list[index] = guess
-                    word_completion = "".join(word_as_list)
-                    if "_" not in word_completion:
-                        guessed = True
-
-                    elif len(guess) == len(word) and guess.isalpha():
-                        if guess in guessed_words:
-                            print("You already guessed the word", guess)
-                        elif guess != word:
-                            print(guess, "is not the word.")
-                            tries -= 1
-                            guessed_words.append(guess)
-                        else:
-                            guessed = True
-                            word_completion = word
-
+    def play_hangman(self):
+        """
+        While user has lost less than 7 lives,
+        requests a letter from user & validates input to ensure it is a letter
+        checks if the letter is in the word and if it has been guessed already.
+        Gives user appropraite feedback.
+        Breaks loop when user has correctly guessed all letters
+        """
+        while self.stage < 6:
+            self.display_hangman()
+            guess = input(f'{Fore.YELLOW}Choose a letter: {Style.RESET_ALL}').lower().strip()  # noqa
+            print('\n')
+            if guess.isalpha() and len(guess) == 1:
+                if guess not in self.word:
+                    if guess in self.guessed_letters:
+                        print(f'You already guessed {guess}, try again')
+                        print('\n')
                     else:
-                        print("Not a valid guess.")
-                        print(display_hangman(tries))
-                        print(word_completion)
-                        print("/n")
+                        print(f'{Fore.RED}{guess} is not in the word, try again{Style.RESET_ALL}')  # noqa
+                        print('\n')
+                        self.stage += 1
+                        self.guessed_letters.append(guess)
+                elif guess.isalpha() and guess in self.word:
+                    if guess in self.guessed_letters:
+                        print(f'You already guessed {guess}, try again')
+                        print('\n')
+                    else:
+                        print(f'{Fore.GREEN}{guess} is in the word!{Style.RESET_ALL}')  # noqa
+                        print('\n')
+                        self.guessed_letters.append(guess)
+                        # code for replacing dashes with letters adapted from  # noqa
+                        # https://github.com/kiteco/python-youtube-code/blob/master/build-hangman-in-python/hangman.py
+                        word_as_list = list(self.progress)
+                        indices = [i for i, letter in enumerate(self.word) if letter == guess]  # noqa
+                        for index in indices:
+                            word_as_list[index] = guess
+                            self.progress = "".join(word_as_list)
+                        if "-" not in self.progress:
+                            print(f'{Fore.GREEN}Congrats! You correctly guessed the answer: {self.word}{Style.RESET_ALL}')  # noqa
+                            print('\n')
+                            self.games_won += 1
+                            break
 
-                        if guessed:
-                            print("Congrats, you guessed the word! You win!")
-                        else:
-                            print(
-                                "Sorry, you ran out of tries. The word was " + word + ". Maybe next time!") # noqa
+            elif guess.isalpha() and guess == self.word:
+                print(f'{Fore.GREEN}Congrats! You correctly guessed the answer: {self.word}{Style.RESET_ALL}')  # noqa
+                print('\n')
+                self.games_won += 1
+                break
+
+            elif guess.isalpha() and guess not in self.word and guess in self.guessed_words:  # noqa
+                print(f'You already guessed {guess}, try again')
+                print('\n')
+
+            elif guess.isalpha() and guess not in self.word and guess not in self.guessed_words:  # noqa
+                print(f'{Fore.RED}{guess} is not the word, try again{Style.RESET_ALL}')  # noqa
+                print('\n')
+                self.stage += 1
+                self.guessed_words.append(guess)
+                print('\n')
+            else:
+                print('Invalid input \n')
+        if self.stage >= 6:
+            print(Fore.CYAN + HANGMAN_PICS[self.stage])
+            print('\n')
+            print(f'{Fore.RED}Game Over! The word was {self.word}{Style.RESET_ALL}')  # noqa
+            print('\n')
+        self.play_again()
+
+    def play_again(self):
+        """
+        Asks user if they want to play again by entering Y or N
+        Calls display_hangman function while user wants to play again
+        """
+        play = input(f'{Fore.YELLOW}Would you like to play again? (Y/N)').strip().upper()  # noqa
+        print('\n')
+        if play == 'Y':
+            self.stage = 0
+            self.guessed_letters = []
+            self.guessed_words = []
+            self.word = random.choice(words)
+            self.progress = '-' * len(self.word)
+            self.games_played += 1
+            self.play_hangman()
+        elif play == 'N':
+            self.games_played += 1
+            print('Thanks for playing! \n')
+            print(f'You won {self.games_won} out of {self.games_played} games')
+        else:
+            print('Invalid choice \n')
+            self.play_again()
 
 
-def display_hangman(tries):
+def main():
+    """
+    Run hangman game functions
+    """
+    game = Hangman()
+    game.play_hangman()
 
-    return stages[tries]
+
+print(f"{Fore.YELLOW}Lets play Hangman!{Style.RESET_ALL}")
+main()
