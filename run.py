452691370b9d6db0481158ed61d349ef90bca66a
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2022-04-08 22:43:22 +0000

    fixed commmenting

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ class Hangman:
         Displays the hangman image relevant to the amount of lives remaining
         Dsiplays the random word with letters represented by dashes
         """
-        print(Fore.CYAN + HANGMAN_PICS[self.stage])
+        print(+ HANGMAN_PICS[self.stage])
         print('\n')
         print(self.progress + Style.RESET_ALL)
         print('\n')
@@ -45,7 +45,7 @@ class Hangman:
         """
         while self.stage < 6:
             self.display_hangman()
-            guess = input(f'{Fore.YELLOW}Choose a letter: {Style.RESET_ALL}').lower().strip()  # noqa
+            guess = input(f'Choose a letter: {Style.RESET_ALL}').lower().strip()  # noqa
             print('\n')
             if guess.isalpha() and len(guess) == 1:
                 if guess not in self.word:
@@ -53,7 +53,7 @@ class Hangman:
                         print(f'You already guessed {guess}, try again')
                         print('\n')
                     else:
-                        print(f'{Fore.RED}{guess} is not in the word, try again{Style.RESET_ALL}')  # noqa
+                        print(f'{guess} is not in the word, try again{Style.RESET_ALL}')  # noqa
                         print('\n')
                         self.stage += 1
                         self.guessed_letters.append(guess)
@@ -62,7 +62,7 @@ class Hangman:
                         print(f'You already guessed {guess}, try again')
                         print('\n')
                     else:
-                        print(f'{Fore.GREEN}{guess} is in the word!{Style.RESET_ALL}')  # noqa
+                        print(f'{guess} is in the word!{Style.RESET_ALL}')  # noqa
                         print('\n')
                         self.guessed_letters.append(guess)
                         # code for replacing dashes with letters adapted from  # noqa
@@ -73,13 +73,13 @@ class Hangman:
                             word_as_list[index] = guess
                             self.progress = "".join(word_as_list)
                         if "-" not in self.progress:
-                            print(f'{Fore.GREEN}Congrats! You correctly guessed the answer: {self.word}{Style.RESET_ALL}')  # noqa
+                            print(f'Congrats! You correctly guessed the answer: {self.word}{Style.RESET_ALL}')  # noqa
                             print('\n')
                             self.games_won += 1
                             break
 
             elif guess.isalpha() and guess == self.word:
-                print(f'{Fore.GREEN}Congrats! You correctly guessed the answer: {self.word}{Style.RESET_ALL}')  # noqa
+                print(f'Congrats! You correctly guessed the answer: {self.word}{Style.RESET_ALL}')  # noqa
                 print('\n')
                 self.games_won += 1
                 break
@@ -89,7 +89,7 @@ class Hangman:
                 print('\n')
 
             elif guess.isalpha() and guess not in self.word and guess not in self.guessed_words:  # noqa
-                print(f'{Fore.RED}{guess} is not the word, try again{Style.RESET_ALL}')  # noqa
+                print(f'{guess} is not the word, try again{Style.RESET_ALL}')  # noqa
                 print('\n')
                 self.stage += 1
                 self.guessed_words.append(guess)
@@ -97,9 +97,9 @@ class Hangman:
             else:
                 print('Invalid input \n')
         if self.stage >= 6:
-            print(Fore.CYAN + HANGMAN_PICS[self.stage])
+            print(+ HANGMAN_PICS[self.stage])
             print('\n')
-            print(f'{Fore.RED}Game Over! The word was {self.word}{Style.RESET_ALL}')  # noqa
+            print(f'Game Over! The word was {self.word}{Style.RESET_ALL}')  # noqa
             print('\n')
         self.play_again()
 
@@ -108,7 +108,7 @@ class Hangman:
         Asks user if they want to play again by entering Y or N
         Calls display_hangman function while user wants to play again
         """
-        play = input(f'{Fore.YELLOW}Would you like to play again? (Y/N)').strip().upper()  # noqa
+        play = input(f'Would you like to play again? (Y/N)').strip().upper()  # noqa
         print('\n')
         if play == 'Y':
             self.stage = 0
@@ -135,5 +135,5 @@ def main():
     game.play_hangman()
 
 
-print(f"{Fore.YELLOW}Lets play Hangman!{Style.RESET_ALL}")
+print(f"Lets play Hangman!{Style.RESET_ALL}")
 main()
